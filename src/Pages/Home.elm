1744953b4232fa1fea732eaaf7a45e2037a7fc65
Maurizio <maury.vacca.1984@gmail.com>
2025-05-19 12:36:21 +0200

    refactor: use typography utils, improve page layout and add links

--- src/Pages/Home.elm
+++ src/Pages/Home.elm
@@ -1,23 +1,27 @@
 module Pages.Home exposing (..)
 
-import Html exposing (Html, article, div, h1, h2, img, section, text)
+import Html exposing (Html, a, article, div, h1, h2, img, section, text)
 import Html.Attributes
 import Shared
+import Ui.Typography
 
 
 view : Shared.Model -> Html msg
 view model =
     section [ Html.Attributes.class "flex flex-col gap-4" ]
-        [ h1
-            [ Html.Attributes.class "text-4xl pb-6 text-amber-950 tracking relative before:absolute before:content-[' '] before:bottom-4 before:left-0 before:w-10 before:h-1 before:bg-amber-400"
-            ]
-            [ text "Categories" ]
-        , div [ Html.Attributes.class "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10" ]
+        [ Ui.Typography.mainTitle [ text "Categories" ]
+        , Ui.Typography.introParagraph
+            [ text """Welcome to our recipe categories — your gateway to culinary inspiration! Whether you're craving comforting classics, exploring international flavors,
+            or searching for quick weeknight dinners, we've organized our recipes to make your cooking journey easier and more enjoyable.
+            Dive in and discover delicious ideas tailored to every taste, occasion, and skill level.""" ]
+        , div [ Html.Attributes.class "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 mt-6" ]
             (List.map
                 (\category ->
-                    article [ Html.Attributes.class "flex flex-col gap-4 overflow-hidden pb-4 bg-white border-b-3 border-amber-400" ]
-                        [ img [ Html.Attributes.src category.thumb, Html.Attributes.alt category.description, Html.Attributes.class "object-cover w-full h-auto bg-gray-100" ] []
-                        , h2 [ Html.Attributes.class "font-bold text-2xl px-4 tracking-wide text-amber-900" ] [ text category.name ]
+                    article [ Html.Attributes.class "flex flex-col gap-4 overflow-hidden bg-white border-b-[3px] border-amber-400 pb-4" ]
+                        [ a [ Html.Attributes.href ("/category/" ++ String.toLower category.name) ]
+                            [ img [ Html.Attributes.src category.thumb, Html.Attributes.alt category.description, Html.Attributes.class "object-cover w-full h-auto bg-gray-100" ] []
+                            , h2 [ Html.Attributes.class "font-bold text-2xl px-4 pt-2 tracking-wide text-amber-900" ] [ text category.name ]
+                            ]
                         ]
                 )
                 model.categories
