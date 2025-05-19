ed06cb269ea5d17b3d89eb95cca7774d8bbdab53
Maurizio <maury.vacca.1984@gmail.com>
2025-05-19 12:30:15 +0200

    feat: add basic typography utils

---
+++ src/Ui/Typography.elm
@@ -0,0 +1,18 @@
+module Ui.Typography exposing (..)
+
+import Html exposing (Html, h1, p)
+import Html.Attributes
+
+
+{-| A heading with emphasis.
+-}
+mainTitle : List (Html msg) -> Html msg
+mainTitle children =
+    h1
+        [ Html.Attributes.class "text-4xl pb-6 text-amber-950 tracking relative before:absolute before:content-[' '] before:bottom-4 before:left-0 before:w-10 before:h-1 before:bg-amber-400" ]
+        children
+
+
+introParagraph : List (Html msg) -> Html msg
+introParagraph children =
+    p [ Html.Attributes.class "leading-8 text-amber-950" ] children
