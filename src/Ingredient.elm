baa62008895f0e11af366030c9a998e3823fa7d2
Maurizio <maury.vacca.1984@gmail.com>
2025-05-20 10:42:06 +0200

    feat: add ingredients decoder

--- src/Ingredient.elm
+++ src/Ingredient.elm
@@ -1,9 +1,28 @@
 module Ingredient exposing (..)
 
+import Http
+import Json.Decode
+import Json.Decode.Pipeline as JDPipeline
+import Utils
+
 
 type alias Ingredient =
     { id : Int
     , name : String
-    , description : String
-    , variety : String
     }
+
+
+type Msg
+    = Receive (Result Http.Error (List Ingredient))
+
+
+getIngredients : Cmd Msg
+getIngredients =
+    Http.get { url = Utils.apiUrl ++ "list.php?i=list", expect = Http.expectJson Receive (Json.Decode.field "meals" (Json.Decode.list ingredientDecoder)) }
+
+
+ingredientDecoder : Json.Decode.Decoder Ingredient
+ingredientDecoder =
+    Json.Decode.succeed Ingredient
+        |> JDPipeline.required "idIngredient" Utils.stringToInt
+        |> JDPipeline.required "strIngredient" Json.Decode.string
